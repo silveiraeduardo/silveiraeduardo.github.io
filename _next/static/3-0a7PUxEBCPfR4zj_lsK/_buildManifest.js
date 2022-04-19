efe920483a402bcb35714bc392614024458e85de
KsAkira10 <akirauehara93@gmail.com>
2022-04-19 21:57:11 +0000

    Deploying to gh-pages from @ silveiraeduardo/silveiraeduardo.github.io@9c8267a78732ba86b163abeb0b7e75018d3d02a3 🚀

--- _next/static/3-0a7PUxEBCPfR4zj_lsK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/20c4376c92000d5e.css","static/chunks/pages/index-577f2fc14892d430.js"],"/_error":["static/chunks/pages/_error-ec7b174845522495.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
