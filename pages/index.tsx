17a0c7796ce6fba5e2c214d0e796d13344e28236
Lucas Akira Uehara <lucas.uehara@riachuelo.com.br>
2022-04-19 19:30:29 -0300

    feat: ajuste de description

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,18 +7,21 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Eduardo Silveira • UX designer</title>
-        <meta name="description" content="soluções e interfaces que resolvem problemas" />
+        <meta name="description" content="Soluções e interfaces que resolvem problemas, focados em UX design." />
         <link rel="canonical" href={process.env.NEXT_PUBLIC_URL} />
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:url" content={process.env.NEXT_PUBLIC_URL} />
         <meta name="twitter:title" content="eduardo silveira" />
-        <meta name="twitter:description" content="soluções e interfaces que resolvem problemas" />
+        <meta
+          name="twitter:description"
+          content="Soluções e interfaces que resolvem problemas, focados em UX design."
+        />
         <meta name="twitter:image" content={`${process.env.NEXT_PUBLIC_URL}/android-chrome-192x192.png`} />
         <meta name="twitter:creator" content="@oisoudesigner" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="eduardo silveira" />
-        <meta property="og:description" content="soluções e interfaces que resolvem problemas" />
+        <meta property="og:description" content="Soluções e interfaces que resolvem problemas, focados em UX design." />
         <meta property="og:site_name" content="eduardo silveira" />
         <meta property="og:url" content={process.env.NEXT_PUBLIC_URL} />
         <meta property="og:image" content={`${process.env.NEXT_PUBLIC_URL}/apple-touch-icon.png`} />
