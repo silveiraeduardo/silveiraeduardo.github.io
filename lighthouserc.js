279c372c19b672605424d53f448cbb96c037a82c
Lucas Akira Uehara <akirauehara@hotmail.com>
2022-04-22 16:10:52 -0300

    Create lighthouserc.js

---
+++ lighthouserc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['http://localhost:3000/'],
+      startServerCommand: 'pnpm http-server ./out/ -p3000 -gb',
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
