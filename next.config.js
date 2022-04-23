4601d627bb2936c67791e5d858e25ac2e4115fa8
Lucas Akira Uehara <akirauehara@hotmail.com>
2022-04-23 11:28:53 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,49 @@ const withPWA = require('next-pwa');
 
 const isProd = process.env.NODE_ENV === 'production';
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self';
+  child-src example.com;
+  style-src 'self' example.com;
+  font-src 'self';
+`;
+
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compress: true,
@@ -32,6 +75,15 @@ const nextConfig = {
     path: '',
   },
   assetPrefix: isProd ? process.env.PUBLIC_URL : '',
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = withPWA(withVanillaExtract(nextConfig));
